417aa6a215023f2da8bd3eebfd4725c7e12460c0
fhrstv <mazenfathi417@gmail.com>
2024-07-30 19:01:31 +0300

    Update random.js

--- random.js
+++ random.js
@@ -2,10 +2,10 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('randomButton').addEventListener('click', function() {
         // قائمة الروابط
         const links = [
-            'watch/movies/2012',
-            'watch/movies/2012',
-            'watch/movies/2012',
-            'watch/movies/2012'
+            'Anime',
+            'Series',
+            'Movies',
+            
         ];
 
         // اختيار رابط عشوائي
