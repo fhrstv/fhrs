053eaf1f9ef21e474f880966bd5a08d972975291
fhrstv <mazenfathi417@gmail.com>
2024-08-11 04:22:10 +0300

    Update random.js

--- random.js
+++ random.js
@@ -7,13 +7,13 @@ async function getRandomHtmlFile() {
             const response = await fetch(`https://api.github.com/repos/fhrstv/fhrs/contents/${folder}`);
             const data = await response.json();
 
-            // تأكد من أن البيانات التي تم الحصول عليها هي مصفوفة من الملفات
             if (Array.isArray(data)) {
                 data.forEach(file => {
                     if (file.type === 'file' && file.name.endsWith('.html')) {
                         // بناء الرابط لعرض الصفحة عبر GitHub Pages
                         const pageUrl = `https://fhrstv.github.io/fhrs/${folder}/${file.name}`;
-                        allFiles.push(pageUrl);
+                        const cleanUrl = pageUrl.replace('.html', ''); // إخفاء .html من الرابط
+                        allFiles.push(cleanUrl);
                     }
                 });
             } else {
