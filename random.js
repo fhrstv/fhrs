24561ce971a0c9243f51672f6d7a6bae60e55cc3
fhrstv <mazenfathi417@gmail.com>
2024-08-11 21:07:38 +0300

    Update random.js

--- random.js
+++ random.js
@@ -4,26 +4,31 @@ async function getRandomHtmlFile() {
 
     for (const folder of folders) {
         try {
-            const response = await fetch(`https://api.github.com/repos/fhrstv/fhrs/contents/${folder}`);
-            const data = await response.json();
-
-            data.forEach(file => {
-                if (file.type === 'file' && file.name.endsWith('.html')) {
-                    // إنشاء الرابط لعرض الصفحة عبر GitHub Pages
-                    const pageUrl = `/${folder}/${file.name.replace('.html', '')}`;
-                    allFiles.push(pageUrl);
+            const xhr = new XMLHttpRequest();
+            xhr.open('GET', `https://api.github.com/repos/fhrstv/fhrs/contents/${folder}`, true);
+            xhr.onload = function() {
+                if (xhr.status === 200) {
+                    const data = JSON.parse(xhr.responseText);
+                    data.forEach(file => {
+                        if (file.type === 'file' && file.name.endsWith('.html')) {
+                            const pageUrl = `/${folder}/${file.name.replace('.html', '')}`;
+                            allFiles.push(pageUrl);
+                        }
+                    });
+                    if (allFiles.length > 0) {
+                        const randomFile = allFiles[Math.floor(Math.random() * allFiles.length)];
+                        window.location.href = randomFile;
+                    } else {
+                        console.error('No HTML files found.');
+                    }
+                } else {
+                    console.error(`Error fetching files from ${folder}:`, xhr.statusText);
                 }
-            });
+            };
+            xhr.send();
         } catch (error) {
             console.error(`Error fetching files from ${folder}:`, error);
         }
-    }
-
-    if (allFiles.length > 0) {
-        const randomFile = allFiles[Math.floor(Math.random() * allFiles.length)];
-        window.location.href = randomFile;
-    } else {
-        console.error('No HTML files found.');
     }
 }
 
