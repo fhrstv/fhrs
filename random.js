2f6d8929d00a76ee55d3518e2a943107115b0b9b
fhrstv <mazenfathi417@gmail.com>
2024-08-16 04:07:58 +0300

    Update random.js

--- random.js
+++ random.js
@@ -43,7 +43,11 @@ loadAllHtmlFiles();
 // ربط زر "الاختيار العشوائي" بالدالة
 document.getElementById('randomButton').addEventListener('click', getRandomHtmlFile);
 
-// تحقق من أن الكود يتم تنفيذه فقط عند الحاجة
+// تحقق من أن الكود يتم تنفيذه فقط عند الحاجة// تحقق من أن السكربت يتم تحميله مباشرة، وليس من خلال عملية التوجيه الصحيحة
 if (window.location.pathname === '/random.js') {
+    // حول المستخدم إلى صفحة 404
     window.location.replace('/404.html');
+} else {
+    // إذا لم يكن في المسار المحدد، نفذ الكود الخاص بـ random.js
+    // يمكن أن تضع هنا أي كود تريد تنفيذه إذا كان يتم تحميل السكربت بطريقة صحيحة
 }
